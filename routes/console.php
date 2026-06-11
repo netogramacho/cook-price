b7695223a14272fe182671f2256295c6d573fa3e
Paulo Gramacho <neto.paulo.po@gmail.com>
2026-06-11 00:29:51 -0300

    fix: restore expire-subscriptions schedule to daily

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:expire-subscriptions')->everyMinute();
+Schedule::command('app:expire-subscriptions')->daily();
